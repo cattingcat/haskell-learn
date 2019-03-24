f8bee3d87bb778f30eb9f5ea0e8a175fc4a3a387
Mark Martynov <mark.martynov@team.wrike.com>
2019-03-24 21:02:28 +0300

    add randot tree without implicit state passing

--- Sub/Trees/RandomTree.hs
+++ Sub/Trees/RandomTree.hs
@@ -6,16 +6,27 @@ import Sub.MyState
 
 randomTree :: Int -> Int -> Random (Tree Int)
 randomTree d c = State $ \s -> let
-    (depth, s0) = runState (limIntRnd d) s
-    (child, s1) = runState (limIntRnd c) s0
-    (value, s2) = runState (limIntRnd 100) s1
+    (depth:child:value:[], s2) = runState (rndLimits [d, c, 100]) s
     (children, s3) = if d == 1 then ([], s2)
         else runState (times child (randomTree (d - 1) c)) s2
     in (Node value children, s3)
+
+
+randomT :: Int -> Int -> Random (Tree Int)
+randomT d c = do
+    tmp <- rndLimits [d, c, 100]
+    let (depth:child:value:[]) = tmp
+    children <- if d == 1 then pure ([]) else times child (randomT (d - 1) c)
+    return $ Node value children
+
 
 times :: Monad m => Int -> m a -> m [a]
 times 0 s = pure []
 times l s = s >>= (\a -> (times (l - 1) s) >>= (\c -> pure (a:c)))
 
-runRndTree = runState (randomTree 5 5) 0.534
+rndLimits :: [Int] -> Random [Int]
+rndLimits [] = pure []
+rndLimits (lim:rest) = limIntRnd lim >>= \r -> (rndLimits rest) >>= \l -> pure (r:l)
+
+runRndTree = runState (randomT 5 5) 0.534
 
